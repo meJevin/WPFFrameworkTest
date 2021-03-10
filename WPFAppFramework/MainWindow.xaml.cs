7e733556094fdf1d121c6801f45bc569feeb5c9a
Michael <mejevin@gmail.com>
2021-03-10 11:27:52 +0300

    1.0.0

--- WPFAppFramework/MainWindow.xaml.cs
+++ WPFAppFramework/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Squirrel;
+
 namespace WPFAppFramework
 {
     /// <summary>
@@ -20,9 +22,42 @@ namespace WPFAppFramework
     /// </summary>
     public partial class MainWindow : Window
     {
+        UpdateManager manager;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            manager = await UpdateManager
+                .GitHubUpdateManager(@"https://github.com/meJevin/WPFFramworkTest");
+
+            CurrentVersionTextBox.Text = manager.CurrentlyInstalledVersion().ToString();
+        }
+
+        private async void CheckForUpdatesButton_Click(object sender, RoutedEventArgs e)
+        {
+            var updateInfo = await manager.CheckForUpdate();
+
+            if (updateInfo.ReleasesToApply.Count > 0)
+            {
+                UpdateButton.IsEnabled = true;
+            }
+            else
+            {
+                UpdateButton.IsEnabled = false;
+            }
+        }
+
+        private async void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            await manager.UpdateApp();
+
+            MessageBox.Show("Updated succesfuly!");
         }
     }
 }
